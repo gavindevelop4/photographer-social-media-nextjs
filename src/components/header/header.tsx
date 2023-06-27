dd3291e9c1ab1626c94f93308d24284220fae063
Gavin Chan <gavinkelsier2018@gmail.com>
2023-06-27 11:28:07 -0400

    Alter: header scroll transition

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,36 @@
+'use client';
+
+import { useEffect, useState, useRef } from 'react';
 import styles from './header.module.scss';
 
 export default function Header() {
+  const [show, setShow] = useState<boolean>(true);
+  const ref = useRef<boolean>();
+  ref.current = show;
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = (event: Event) => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (ref.current && scrollTop > 50) {
+      setShow(false);
+    }
+
+    if (!ref.current && scrollTop < 50) {
+      setShow(true);
+    }
+  };
+
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${show ? styles.show : ''}`}>
       <h1 className={styles.logo}>Bokeh</h1>
     </header>
   )
